4ee45d2b8782bbedab3affc4fdc63a1da14f52f1
Matheus Oliveira <contamatheusprofissional@gmail.com>
2025-11-01 15:32:01 -0300

    feat: creating a base entity and implementing it in the message entity

--- src/messages/entities/message.entity.ts
+++ src/messages/entities/message.entity.ts
@@ -1,7 +1,17 @@
-export class MessageEntity {
-  id: number;
+import { BaseEntity } from 'src/common/entities/base.entity';
+import { Column, Entity } from 'typeorm';
+
+@Entity('messages')
+export class MessageEntity extends BaseEntity {
+  @Column({ type: 'text' })
   content: string;
+
+  @Column()
   sender: string;
+
+  @Column()
   recipient: string;
+
+  @Column({ default: false })
   read: boolean;
 }
