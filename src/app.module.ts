be2ab469d001fa7f7eb71ccf8e65d3b729386cd2
Matheus Oliveira <contamatheusprofissional@gmail.com>
2025-11-01 14:53:18 -0300

    feat: basic typeorm settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,38 @@
 import { Module } from '@nestjs/common';
 import { MessagesModule } from './messages/messages.module';
 import { HealthModule } from './health/health.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 // nest generate resource <resource> (nest g res <resource>)
 
+/* 
+  npm i @nestjs/typeorm typeorm
+  npm i @nestjs/config
+  npm i  mysql2
+*/
+
 @Module({
-  imports: [MessagesModule, HealthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DATABASE_HOST'),
+        port: configService.get<number>('DATABASE_PORT'),
+        username: configService.get('DATABASE_USERNAME'),
+        password: configService.get('DATABASE_PASSWORD'),
+        database: configService.get('DATABASE_NAME'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+    MessagesModule,
+    HealthModule,
+  ],
   controllers: [],
   providers: [],
 })
