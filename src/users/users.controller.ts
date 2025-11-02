e4a47fc426e9b870b0d87f1c950708aa67fa09e2
Matheus Oliveira <contamatheusprofissional@gmail.com>
2025-11-02 08:04:29 -0300

    feat: endpoint to find all users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { UserEntity } from './entities/user.entity';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  public findAll(): Promise<UserEntity[]> {
+    return this.usersService.findAll();
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly usersRepository: Repository<UserEntity>,
+  ) {}
+
+  public async findAll(): Promise<UserEntity[]> {
+    return await this.usersRepository.find();
+  }
+}
