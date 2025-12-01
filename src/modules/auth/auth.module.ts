3497d5d0e9b432ded4c3fd4336b8bfdb33369508
Matheus Oliveira <contamatheusprofissional@gmail.com>
2025-12-01 19:12:57 -0300

    feat: basic jwt settings

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,11 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { CryptographyModule } from 'src/common/modules/cryptography/cryptography.module';
 import { UsersModule } from '../users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
+// npm i @nestjs/jwt
+
 @Module({
-  imports: [UsersModule, CryptographyModule],
+  imports: [
+    UsersModule,
+    CryptographyModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get('JWT_SECRET'),
+        signOptions: {
+          audience: config.get('JWT_AUDIENCE'),
+          issuer: config.get('JWT_ISSUER'),
+          expiresIn: config.get('JWT_TTL'),
+        },
+      }),
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
