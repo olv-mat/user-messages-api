c3f00808476ecbd8fc60b0474240c02084c772b4
Matheus Oliveira <contamatheusprofissional@gmail.com>
2026-01-24 10:07:01 -0300

    docs: add swagger setup

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import helmet from 'helmet';
+import { SwaggerTheme, SwaggerThemeNameEnum } from 'swagger-themes';
 import { AppModule } from './app.module';
 import { ErrorLoggingInterceptor } from './common/interceptors/error-logging.interceptor';
 import { RequestTimeInterceptor } from './common/interceptors/request-time.interceptor';
@@ -33,6 +35,26 @@ async function bootstrap() {
     app.use(helmet()); // Set Security-Related HTTP Headers
     app.enableCors({}); // Allow Requests From Other Domains
   }
+
+  const documentBuilder = new DocumentBuilder()
+    .setTitle('Basic Chat API')
+    .setDescription(
+      `A basic real-time chat API built with NestJS, featuring user and message modules, 
+      WebSocket-based communication, and secure JWT authentication with policy-based authorization.`.trim(),
+    )
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, documentBuilder);
+  const theme = new SwaggerTheme();
+  SwaggerModule.setup('api', app, document, {
+    customCss: theme.getBuffer(SwaggerThemeNameEnum.DARK),
+    swaggerOptions: {
+      persistAuthorization: true,
+      tagsSorter: 'alpha',
+      operationsSorter: 'alpha',
+    },
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
